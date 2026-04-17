22042543d54bceca3ce8ae8f8e60518cd5f8bb88
Al-amin <111284227+Al-amin1911@users.noreply.github.com>
2026-04-17 16:40:20 +0100

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        char_count_s1= {}
+        char_count_s2 = defaultdict(dict)
+        for char in s1:
+            if char not in char_count_s1.keys():
+                char_count_s1[char] = 1
+            else:
+                char_count_s1[char] += 1
+        while l < len(s2)-len(s1)+1:
+            r = l+len(s1)
+            print(l, r)
+            for char in s2[l:r]:
+                if char not in char_count_s2[l].keys():
+                    char_count_s2[l][char] = 1
+                else:
+                    char_count_s2[l][char] += 1
+            print(char_count_s1, char_count_s2[l])
+            if char_count_s1 == char_count_s2[l]:
+                return True
+            l += 1
+        return False
